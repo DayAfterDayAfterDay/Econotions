be0ee3317f3055b459bdbe42478e58931ef47b49
DayAfterDayAfterDay <79156931+DayAfterDayAfterDay@users.noreply.github.com>
2023-08-13 18:17:10 +0200

    Add files via upload

---
+++ mean_absorbing_time.R
@@ -0,0 +1,69 @@
+library(boot)
+
+#   ____________________________________________________________________________
+#   Balanced RW                                                             ####
+
+
+tot_walks <- 3000
+length_walk <- 2500 # 2500 steps
+
+stop <- c() #store the number of steps before hitting an absorbing barrier
+
+for(walk in 1:tot_walks){
+    
+    x <- 30 + cumsum(sample(c(-1, 1), length_walk, TRUE)) # simulate a balanced random walk
+                                                          # of 2500 steps starting from 30
+    
+    counter <- 1
+    
+    while(x[counter]>0 & x[counter]<50 & counter <=2500){
+    
+      counter <- counter +1 # update the number of steps until we reach 0 or 50
+      
+    }
+    
+
+    stop <- c(stop, counter) # store the number of steps for each walk
+  
+  
+}
+
+boot(stop, statistic = function(x, inds) mean(x[inds]), R = 100) # run a bootstrap on the mean
+
+'''
+In our example k = 30 and N=0 so Lk=k(N-k)=600 agrees with the results from our simulation
+'''
+
+
+#   ____________________________________________________________________________
+#   Unbalanced RW                                                           ####
+
+tot_walks <- 3000
+length_walk <- 2500 # 2500 steps
+
+stop <- c() #store the number of steps before hitting an absorbing barrier
+
+for(walk in 1:tot_walks){
+  
+  x <- 30 + cumsum(sample(c(-1, 1), length_walk, TRUE, prob = c(0.45,0.55))) # simulate a unbalanced random walk
+                                                                             # of 2500 steps starting from 30
+                                                                             # with p(xn=+1) = 0.55
+
+  counter <- 1
+  
+  while(x[counter]>0 & x[counter]<50 & counter <=2500){
+    
+    counter <- counter +1 # update the number of steps until we reach 0 or 50
+    
+  }
+  
+  stop <- c(stop, counter) # store the number of steps for each walk
+  
+  
+}
+
+boot(stop, statistic = function(x, inds) mean(x[inds]), R = 100)  # run a bootstrap on the mean
+
+'''
+In our example k = 30 and N=0 so Lk=200 agrees with the results from our simulation
+'''
